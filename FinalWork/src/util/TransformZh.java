6e71d2edcd5e6639ce848c7383bb0682cf28e1cb
JJPAOVERSKY <57521892+JJPAOVERSKY@users.noreply.github.com>
2020-12-24 00:18:14 +0800

    Update TransformZh.java

--- FinalWork/src/util/TransformZh.java
+++ FinalWork/src/util/TransformZh.java
@@ -2,10 +2,8 @@ package util;
 
 public class TransformZh {
 	public static String unicodeToCn(String unicode) {
-        /** 以 \ u 分割，因为java注释也能识别unicode，因此中间加了一个空格*/
         String[] strs = unicode.split("\\\\u");
         String returnStr = "";
-        // 由于unicode字符串以 \ u 开头，因此分割出的第一个字符是""。
         for (int i = 1; i < strs.length; i++) {
             returnStr += (char) Integer.valueOf(strs[i], 16).intValue();
         }
